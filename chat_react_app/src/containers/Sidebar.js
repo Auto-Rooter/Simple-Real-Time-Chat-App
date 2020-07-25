fc521addbb5f7a61f02668b3eb0a789bdf1738ec
Autorooter <hadi.assalem@gmail.com>
2020-07-26 00:08:14 +0200

    add Sidebar component

---
+++ chat_react_app/src/containers/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropType from 'prop-types'
+
+const Sidebar = ({users}) => (
+    <aside id="sidebar" className="sidebar">
+        <ul>
+            {users.map(user => {
+                <li key={user.id}> {user.name}</li>
+            })}
+        </ul>
+    </aside>
+)
+
+Sidebar.PropType = {
+    users: PropType.arrayOf(
+        PropType.shape({
+            id: PropType.number.isRequired,
+            name: PropType.string.isRequired
+        }).isRequired
+    ).isRequired
+}
+
+export default Sidebar;
